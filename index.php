7bcf30328caa52ccb8fd9e3417a32a8934dcdb42
Samuel Serra <96950152+sserra16@users.noreply.github.com>
2023-02-27 16:39:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro Projeto</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo "Ola mundo"; ?>
+    </h1>
+
+    <h2><?php echo(4 + 5); ?></h2>
+    <h2><?php echo(4 - 5); ?></h2>
+    <h2><?php echo(4 * 5); ?></h2>
+    <h2><?php echo(4 / 5); ?></h2>
+</body>
+
+</html>
